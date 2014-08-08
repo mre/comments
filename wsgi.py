f3d9544c6b8e0b7407f642e0a9dda9fb8077ec9d
Matthias Endler <matthias.endler@trivago.com>
2014-08-08 11:18:57 +0200

    Try a simpler entry point example

--- wsgi.py
+++ wsgi.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python
 import os
-
+ 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
     pass
-
-from app import app, db
-from auth import *
-from admin import admin
-from api import api
-from models import *
-from views import *
-
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
-
-admin.setup()
-api.setup()
-
-if __name__ == '__main__':
-  app.run()
+ 
+from todoapp import app as application
