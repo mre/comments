133ab30d1e13f68882dfe8304052c5bdc6439b18
Matthias Endler <matthias.endler@trivago.com>
2014-08-08 11:10:00 +0200

    Another try on wsgi. That rhymes

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,27 @@
-from app import app, db
+#!/usr/bin/python
+import os
 
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+from app import app, db
 from auth import *
 from admin import admin
 from api import api
 from models import *
 from views import *
 
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
 admin.setup()
 api.setup()
 
-
 if __name__ == '__main__':
-    app.run()
+  app.run()
